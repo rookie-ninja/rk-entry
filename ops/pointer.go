bfc0964184916fbe26a951a73d3cda5ba411c333
dongxuny <50768414+dongxuny@users.noreply.github.com>
2022-12-24 23:05:19 +0800

    Add pointer implementation

---
+++ ops/pointer.go
@@ -0,0 +1,105 @@
+package rkops
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/rookie-ninja/rk-entry/v2/cursor"
+	"github.com/rookie-ninja/rk-query"
+	"go.uber.org/zap"
+	"strings"
+	"time"
+)
+
+func NewPointer(payload *rkcursor.CursorPayload) rkcursor.Pointer {
+	return &Pointer{
+		start:     payload.StartTime,
+		parent:    payload.ParentFunc,
+		operation: payload.Operation,
+		event:     payload.Event,
+		logger:    payload.Logger,
+		entryName: payload.EntryName,
+		entryType: payload.EntryType,
+	}
+}
+
+type Pointer struct {
+	start     time.Time
+	parent    string
+	operation string
+	err       error
+	event     rkquery.Event
+	logger    *zap.Logger
+	entryName string
+	entryType string
+}
+
+func (c *Pointer) PrintError(err error) {
+	if err == nil || c.logger == nil {
+		return
+	}
+
+	type stackTracer interface {
+		StackTrace() errors.StackTrace
+	}
+
+	var builder bytes.Buffer
+	builder.WriteString(err.Error())
+	builder.WriteString(fmt.Sprintf("\nStackTrace:"))
+
+	conv, ok := err.(stackTracer)
+
+	if ok {
+		st := conv.StackTrace()
+
+		ss := errors.StackTrace{}
+		for i := range st {
+			frame := st[i]
+			str := fmt.Sprintf("'%+s", frame)
+			if strings.Contains(str, "@") {
+				break
+			}
+
+			ss = append(ss, frame)
+		}
+		builder.WriteString(fmt.Sprintf("%+v", ss))
+		builder.WriteString("\n")
+	}
+
+	c.logger.WithOptions(zap.AddCallerSkip(1)).Error(builder.String())
+}
+
+func (c *Pointer) ObserveError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	c.err = err
+
+	if c.event != nil {
+		c.event.IncCounter(strings.Join([]string{c.operation, "ERROR"}, "."), 1)
+	}
+
+	return err
+}
+
+func (c *Pointer) Release() {
+	elapsedNano := time.Now().Sub(c.start).Nanoseconds()
+
+	success := true
+	if c.err != nil {
+		success = false
+	}
+
+	observer, _ := rkcursor.SummaryVec().GetMetricWithLabelValues(
+		rkcursor.PromLabels().GetValues(c.parent, c.operation, c.entryName, c.entryType, success)...)
+	if observer == nil {
+		return
+	}
+
+	if c.event != nil {
+		c.event.EndTimer(c.operation)
+	}
+
+	observer.Observe(float64(elapsedNano))
+}
